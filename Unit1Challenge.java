6c5b4e19349cd10546a4100c5737cfbb8f32e8e2
Jason R <90394027+The1Riv123@users.noreply.github.com>
2021-10-20 09:30:44 -0400

    damage dealt 0.1
    added scanner

--- Unit1Challenge.java
+++
@@ -1,5 +0,0 @@
-// <Jason Rivers>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/20/21>, <8:51>, v0.0
-
-public class ? {
-    
-}
---
+++ damagedealt.java
@@ -0,0 +1,15 @@
+// <Jason Rivers>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/20/21>, <9:30>, v0.1
+import java.utli.Scanner; 
+public class damagedealt { 
+    Scanner myScanner = newe Scanner(System.in);
+
+    System.out.println("please enter your UserName: ")
+
+    String UserName = myScanner.nextline();
+    
+    System.out.ln("your UserName is:"+ Username);
+
+    int myInt = myScanner.nextnt(); //Scab=n intergers
+    double mydouble = myScanner .nextdouble(); //scan doubles.
+    
+}
