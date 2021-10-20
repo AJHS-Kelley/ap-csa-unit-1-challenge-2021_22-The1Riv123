d97d060406ac5c06ce5cc585ff39c8ab1b5e6ff3
Jason R <90394027+The1Riv123@users.noreply.github.com>
2021-10-20 09:33:59 -0400

    v0.2
    fixed small things in my scanner code

--- damagedealt.java
+++ damagedealt.java
@@ -1,15 +1,16 @@
-// <Jason Rivers>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/20/21>, <9:30>, v0.1
+// <Jason Rivers>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/20/21>, <9:33>, v0.2
 import java.utli.Scanner; 
-public class damagedealt { 
-    Scanner myScanner = newe Scanner(System.in);
+public class DamageDealt { 
+    Scanner myScanner = new Scanner(System.in);
 
     System.out.println("please enter your UserName: ")
 
     String UserName = myScanner.nextline();
     
-    System.out.ln("your UserName is:"+ Username);
+    System.out.println("your UserName is:"+ Username);
 
-    int myInt = myScanner.nextnt(); //Scab=n intergers
-    double mydouble = myScanner .nextdouble(); //scan doubles.
+   
     
 }
+
+
